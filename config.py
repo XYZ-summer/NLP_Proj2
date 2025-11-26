71f0e734e01b5d544128f107b6a7aa022939a16a
ZPL <i-am-zpl@sjtu.edu.cn>
2025-11-26 15:37:39 +0800

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 #API配置
-OPENAI_API_KEY = 
-OPENAI_API_BASE = 
-MODEL_NAME = 
+OPENAI_API_KEY = "sk-0fe3e1a96aee4c228b8eddc74fa70a46"
+OPENAI_API_BASE = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+MODEL_NAME = "qwen3-max"
 OPENAI_EMBEDDING_MODEL = 
 
 # 数据目录配置
-DATA_DIR = 
+DATA_DIR = "./data"
 
 #向量数据库配置
 VECTOR_DB_PATH = "./vector_db"
